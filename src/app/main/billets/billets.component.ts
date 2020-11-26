a517d90229fd6612796bd94658ff50834cf7f409
Gagus <gaetan.bonin@gmail.com>
2020-11-26 09:26:13 +0100

    feat(home page)

---
+++ src/app/main/billets/billets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billets',
+  templateUrl: './billets.component.html',
+  styleUrls: ['./billets.component.scss']
+})
+export class BilletsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
